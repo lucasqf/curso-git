34c48c7ccecbe1f4bc32156e948c4dd8cdcd2c30
Lucas Quirino <lucasquife@gmail.com>
2020-06-17 12:14:59 -0300

    Adicionadas linhas para organizar

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -6,9 +6,11 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------------");
             Console.WriteLine("Ola mundo");
             Console.WriteLine("Como você está?");
             Console.WriteLine("Cúpula");
+            Console.WriteLine("-----------------------------------");
         }
     }
 }
